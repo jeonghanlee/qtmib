3a90a710a58407c2c8204613700b2d88103a9422
rcpteam <rcpteam@a318091f-ed60-4a90-afbd-f9e4fa572904>
2014-05-10 13:50:04 +0000

    1.0 testing

--- mkdist.sh
+++ mkdist.sh
@@ -4,10 +4,7 @@ DIR="$1-$2"
 
 mkdir $DIR
 cp -R src $DIR/.
-cd $DIR
-rm -fr `find . -name .svn`
-rm -fr src/tools src/art
-cd ..
+
 mkdir $DIR/build
 cp configure $DIR/.
 cp Makefile.in $DIR/.
@@ -18,9 +15,13 @@ cp install.sh $DIR/.
 cp COPYING $DIR/.
 cp README $DIR/.
 cp RELNOTES $DIR/.
-cp platform $DIR/.
+cp -R platform $DIR/.
+cd $DIR
+rm -fr `find . -name .svn`
+rm -fr src/tools src/art
+cd ..
 
-tar -cjvf $DIR.tar.bz2 $DIR
+
 rm -fr $DIR
 
 
